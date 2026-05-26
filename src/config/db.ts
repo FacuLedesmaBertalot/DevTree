20f1dc61e283d779a0d59eaac5344e9365689612
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-05-26 19:44:47 -0300

    Añadiendo db y ORM

---
+++ src/config/db.ts
@@ -0,0 +1,14 @@
+import colors from "colors";
+import mongoose from "mongoose";
+
+export const connectDB = async () => {
+    try {
+        const { connection } = await  mongoose.connect(process.env.MONGO_URI);
+        const url = `${connection.host}:${connection.port}`
+
+        console.log( colors.bgCyan.bold( `MongoDB Conectado en ${url}` ));
+    } catch (error) {
+        console.log( colors.bgRed.bold(error.message) );
+        process.exit(1);
+    }
+}
