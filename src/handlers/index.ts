5653ad8e62d1367a1ca8d9ef09ee12bb505f84e4
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-06-01 13:08:13 -0300

    Autenticación de Usuario

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -2,15 +2,10 @@ import type { Request, Response } from 'express';
 import { validationResult } from 'express-validator';
 import slug from 'slug';
 import User from "../models/User";
-import { hashPassword } from '../utils/auth';
+import { checkPassword, hashPassword } from '../utils/auth';
+
 
 export const createAccount = async (req: Request, res: Response) => {
-
-    // Manejar errores
-    let errors = validationResult(req);
-    if (!errors.isEmpty()) {
-        return res.status(400).json({ errors: errors.array() });
-    }
 
     const { email, password } = req.body;
 
@@ -35,4 +30,32 @@ export const createAccount = async (req: Request, res: Response) => {
 
     await user.save();
     res.status(201).send('Registro Creado Correctamente');
+}
+
+
+export const login = async (req: Request, res: Response) => {
+    
+    // Manejar errores
+    let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    // Revisa si el usuario esta registrado
+    const user = await User.findOne({ email });
+    if (!user) {
+        const error = new Error('El Usuario no Existe');
+        return res.status(404).json({ error : error.message });
+    }
+
+    // Comprobar contraseña
+    const isPasswordCorrect = await checkPassword(password, user.password); 
+    if (!isPasswordCorrect) {
+        const error = new Error('La Contraseña es Incorrecta');
+        return res.status(401).json({ error : error.message });
+    }
+
+    res.send('Autenticado...');
 }
--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
-import { createAccount } from './handlers';
+import { createAccount, login } from './handlers';
+import { handleInputErrors } from './middleware/validation';
 
 const router = Router();
 
@@ -19,7 +20,19 @@ router.post('/auth/register',
     body('password')
         .isLength({ min: 8 })
         .withMessage('La contraseña es muy corta, mínimo 8 caracteres'),
+    handleInputErrors,
     createAccount);
+
+router.post('/auth/login', 
+    body('email')
+        .isEmail()
+        .withMessage('Email no válido'),
+    body('password')
+        .notEmpty()
+        .withMessage('La contraseña es obligatoria'),
+    handleInputErrors,
+    login
+)
 
 
 export default router;
