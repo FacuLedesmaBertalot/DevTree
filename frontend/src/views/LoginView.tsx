6517f3b18bfc87e1ea414e7d56502d06b4944d6f
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-06-01 16:49:58 -0300

    Iniciada la Autenticación de Usuarios en el front

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-        <div>LoginView</div>
+        <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
 
-        <nav>
-            <Link to="/auth/register" >
+        <nav className="mt-10">
+            <Link 
+            className="text-center text-white text-lg block"
+            to="/auth/register" 
+            >
                 ¿No tienes Cuenta? Crea una Aquí
             </Link>
         </nav>
