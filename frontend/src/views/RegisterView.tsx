f93267de0d1099352aa23ae84d7238f3f96c5736
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-06-02 13:45:37 -0300

    Añadidas validaciones en el front

--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -1,16 +1,32 @@
 import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
+import type { RegisterForm } from '../types';
+import ErrorMessage from "../components/ErrorMessage";
 
 
 export default function RegisterView() {
-    const { register, watch, handleSubmit, formState: { errors } } = useForm();
+
+    const initialValues: RegisterForm = {
+        name: '',
+        email: '',
+        handle: '',
+        password: '',
+        password_confirmation: ''
+    }
+    const { register, watch, handleSubmit, formState: { errors } } = useForm({ defaultValues: initialValues });
+
+    const password = watch('password');
+
+    const handleRegister = (formData : RegisterForm) => {
+        console.log(formData);
+    }
 
     return (
         <>
             <h1 className="text-4xl text-white font-bold">Crear Cuenta</h1>
 
             <form 
-                onSubmit={() => {}}
+                onSubmit={handleSubmit(handleRegister)}
                 className="bg-white px-5 py-20 rounded-lg space-y-10 mt-10"
             >
                 <div className="grid grid-cols-1 space-y-3">
@@ -20,7 +36,12 @@ export default function RegisterView() {
                         type="text"
                         placeholder="Tu Nombre"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('name', {
+                            required: "El Nombre es Obligatorio"
+                        })}
                     />
+                    { errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage> }
+
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="email" className="text-2xl text-slate-700">E-mail</label>
@@ -29,7 +50,15 @@ export default function RegisterView() {
                         type="email"
                         placeholder="Email de Registro"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('email', {
+                            required: "El Email es Obligatorio",
+                            pattern: {
+                                value: /\S+@\S+\.\S+/,
+                                message: "Email no Válido",
+                            },
+                        })}
                     />
+                    { errors.email && <ErrorMessage>{errors.email.message}</ErrorMessage> }
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="handle" className="text-2xl text-slate-700">Handle</label>
@@ -38,7 +67,11 @@ export default function RegisterView() {
                         type="text"
                         placeholder="Nombre de usuario: sin espacios"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('handle', {
+                            required: "El Handle es Obligatorio"
+                        })}
                     />
+                    { errors.handle && <ErrorMessage>{errors.handle.message}</ErrorMessage> }
                 </div>
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password" className="text-2xl text-slate-700">Password</label>
@@ -47,17 +80,30 @@ export default function RegisterView() {
                         type="password"
                         placeholder="Password de Registro"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password', {
+                            required: "La Contraseña es Obligatoria",
+                            minLength: {
+                                value: 8,
+                                message: "La Contraseña debe ser Mínimo 8 Caracteres"
+                            }
+                        })}
                     />
+                    { errors.password && <ErrorMessage>{errors.password.message}</ErrorMessage> }
                 </div>
 
                 <div className="grid grid-cols-1 space-y-3">
                     <label htmlFor="password_confirmation" className="text-2xl text-slate-700">Repetir Password</label>
                     <input
-                        id="password"
+                        id="password_confirmation"
                         type="password"
                         placeholder="Repetir Password"
                         className="bg-slate-100 border-none p-3 rounded-lg placeholder-slate-400"
+                        {...register('password_confirmation', {
+                            required: "Repetir Contraseña es Obligatorio",
+                            validate: (value) => value === password || 'Las Contraseñas no Coinciden'
+                        })}
                     />
+                    { errors.password_confirmation && <ErrorMessage>{errors.password_confirmation.message}</ErrorMessage> }
                 </div>
 
                 <input
