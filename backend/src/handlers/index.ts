138bca8e20157d74aad241c4b866e93d7cd817b2
FacuLedesmaBertalot <faculedesmabertalot@gmail.com>
2026-06-05 11:03:46 -0300

    Autenticando con JWT

--- backend/src/handlers/index.ts
+++ backend/src/handlers/index.ts
@@ -3,6 +3,7 @@ import { validationResult } from 'express-validator';
 import slug from 'slug';
 import User from "../models/User";
 import { checkPassword, hashPassword } from '../utils/auth';
+import { generateJWT } from '../utils/jwt';
 
 
 export const createAccount = async (req: Request, res: Response) => {
@@ -56,6 +57,8 @@ export const login = async (req: Request, res: Response) => {
         const error = new Error('La Contraseña es Incorrecta');
         return res.status(401).json({ error : error.message });
     }
+
+    generateJWT(user);
 
     res.send('Autenticado...');
 }
