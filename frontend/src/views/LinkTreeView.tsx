24d24b8ef4e7e8d3fb5da46681b1ba2c1f8e16c1
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-07-20 16:17:01 -0300

    Añadiendo la funcionalidad DragNDrop

--- frontend/src/views/LinkTreeView.tsx
+++ frontend/src/views/LinkTreeView.tsx
@@ -97,7 +97,7 @@ export default function LinkTreeView() {
             id: 0,
             enabled: false
           }
-        } else if (link.id > indexToUpdate) {
+        } else if (link.id > indexToUpdate && (indexToUpdate !== 0 && link.id === 1)) {
           return {
             ...link,
             id: link.id - 1
@@ -131,7 +131,7 @@ export default function LinkTreeView() {
 
       <button
         className="bg-lime-500 p-2 text-lg w-full uppercase font-bold text-white rounded-lg cursor-pointer"
-        onClick={() => mutate(user)}
+        onClick={() => mutate(queryClient.getQueryData(['user'])!)}
       >Guardar Cambios</button>
 
     </div>
