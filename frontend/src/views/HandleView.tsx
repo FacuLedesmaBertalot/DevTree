e02b911ca619bc640a3704db866c9412ee915e1c
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-07-22 12:48:09 -0300

    Renderizado para vista

--- frontend/src/views/HandleView.tsx
+++ frontend/src/views/HandleView.tsx
@@ -1,6 +1,7 @@
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
 import { getUserByHandle } from '../api/DevTreeAPI';
+import HandleData from '../components/HandleData';
 
 export default function HandleView() {
 
@@ -14,8 +15,5 @@ export default function HandleView() {
 
   if(isLoading) return <p className='text-center text-white'>Cargando...</p>;
   if(error) return <Navigate to={'/404'} />
-
-  return (
-    <div>HandleView</div>
-  )
+  if(data) return <HandleData data={data}/>
 }
