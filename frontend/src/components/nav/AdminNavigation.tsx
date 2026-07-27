01b6f8057f0b4a07c54f9e29297e4d961570f502
FacuLedesmaBertalot <fanelebe.contacto@gmail.com>
2026-07-27 14:45:19 -0300

    Add useQueryClient

--- frontend/src/components/nav/AdminNavigation.tsx
+++ frontend/src/components/nav/AdminNavigation.tsx
@@ -1,10 +1,18 @@
-
+import { useQueryClient } from '@tanstack/react-query';
 
 export default function AdminNavigation() {
+  const queryClient = useQueryClient();
+
+  const logout = () => {
+    localStorage.removeItem('AUTH_TOKEN');
+    queryClient.invalidateQueries({ queryKey: ['user'] });
+  }
+  
+
   return (
     <button
       className=" bg-lime-500 p-2 text-white uppercase font-black text-xs rounded-lg cursor-pointer hover:bg-lime-700"
-      onClick={() => {}}
+      onClick={logout}
     >
       Cerrar Sesión
     </button>
